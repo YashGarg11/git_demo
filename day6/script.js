4303b96e5271f19d173adef65dcdbd6c57eb420e
git_demo <gargyash98222@gmail.com>
2024-10-14 13:08:46 +0530

    day6

---
+++ day6/script.js
@@ -0,0 +1,13 @@
+function display(){
+    // let p1=document.querySelector("p");
+    let p1=document.querySelectorAll("p"); 
+    p1[0].innerHTML="I am in para";
+    p1[0].style.backgroundColor="red";
+    p1[1].innerHTML="I am in para 2";
+    p1[1].style.backgroundColor="green";
+}
+
+let chanageHeader=()=>{
+    let h1=document.querySelector("h1");
+    h1.textContent="hann Ji Good Morning";
+}
